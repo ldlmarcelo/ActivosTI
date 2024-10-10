f6531aebe287d595c909118b1868fdd87d021db9
Marcelo Luna <ldl_marcelo@hotmail.com>
2024-10-10 14:30:39 -0300

    empiezo a ensayar con vistas. listar dispostivos

--- empresa/activosit/views.py
+++ empresa/activosit/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Dispositivo
+
+def lista_dispositivos(request):
+    search_query = request.GET.get('search', '')
+    
+    if search_query:
+        dispositivos = Dispositivo.objects.filter(nomenclatura__icontains=search_query)
+    else:
+        dispositivos = Dispositivo.objects.all()
+    
+    return render(request, 'lista_dispositivos.html', {'dispositivos': dispositivos, 'search_query': search_query})
