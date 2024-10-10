9886472bea1f7b9cf71b6c9985cc163b7d2bb246
Marcelo Luna <ldl_marcelo@hotmail.com>
2024-10-10 11:21:22 -0300

    séptimo commit

--- empresa/activosit/admin.py
+++ empresa/activosit/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
-from .models import Dispositivo
+from .models import Dispositivo, Historialubicacion, UbicEspecifica, Ubicacion
 
-# Personalización del modelo Dispositivo para el admin
+
+@admin.register(Dispositivo)
 class DispositivoAdmin(admin.ModelAdmin):
-    list_display = ('iddispositivo', 'nomenclatura', 'nrodeserie', 'jiracompra')  # Campos que se mostrarán en la lista
-    search_fields = ('nomenclatura', 'nrodeserie')  # Campos por los que se podrá buscar
-    list_filter = ('jiracompra',)  # Filtros laterales
-    ordering = ('nomenclatura',)  # Orden por defecto
+    list_display = ('nomenclatura', 'nrodeserie')  # Campos a mostrar en la lista
+    search_fields = ('nomenclatura', 'nrodeserie')  # Habilita el campo de búsqueda
+    list_filter = ('nomenclatura',)  # Filtros por número de serie
 
-# Registra el modelo con su configuración personalizada
-admin.site.register(Dispositivo, DispositivoAdmin)
+
+@admin.register(Historialubicacion)
+class HistorialubicacionAdmin(admin.ModelAdmin):
+    list_display = ('dispositivo_iddispositivo', 'ubic_especifica_idubic_especifica', 'fecha')  # Campos a mostrar en la lista
+    search_fields = ('dispositivo_iddispositivo__nomenclatura', 'ubic_especifica_idubic_especifica__ubic_especifica')  # Búsqueda por nombre del dispositivo y ubicación específica
+    list_filter = ('fecha', 'ubic_especifica_idubic_especifica')  # Filtros por fecha y ubicación específica
+
+
+@admin.register(UbicEspecifica)
+class UbicEspecificaAdmin(admin.ModelAdmin):
+    list_display = ('ubic_especifica', 'ubicacion_idubicacion')  # Campos a mostrar en la lista
+    search_fields = ('ubic_especifica', 'ubicacion_idubicacion__ubicacion')  # Búsqueda por nombres de ubicaciones
+    list_filter = ('ubicacion_idubicacion',)  # Filtros por ubicación general
+
+
+@admin.register(Ubicacion)
+class UbicacionAdmin(admin.ModelAdmin):
+    list_display = ('ubicacion',)  # Mostrar solo el campo 'ubicacion'
+    search_fields = ('ubicacion',)  # Búsqueda por nombre de la ubicación
